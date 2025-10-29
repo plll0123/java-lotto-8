27d9d9ba8e15c4d866e756bead64189e0923f692
plll0123 <jsh951227@gmail.com>
2025-10-29 22:01:06 +0900

    feat: 에러 메세지 추가

--- src/main/java/util/ErrorMessage.java
+++ src/main/java/util/ErrorMessage.java
@@ -8,5 +8,8 @@ public class ErrorMessage {
     public static final String INVALID_AMOUNT = "[ERROR] 금액은 천원 이상이어야 합니다.";
     public static final String OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45까지여야 합니다.";
     public static final String INVALID_LOTTO_NUMBER = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public static final String BONUS_NUMBER_DUPLICATE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.";
+
+    public static final String INPUT_ERROR = "[ERROR] 입력값이 존재하지 않습니다. 프로그램을 종료합니다.";
 
 }
