8d53ac02e16d7e40e8695ae656fff2d878521cde
plll0123 <jsh951227@gmail.com>
2025-10-29 20:17:34 +0900

    feat: 에러 메세지 클래스 정의

---
+++ src/main/java/util/ErrorMessage.java
@@ -0,0 +1,11 @@
+package util;
+
+public class ErrorMessage {
+
+    public static final String EMPTY_STRING = "[ERROR] 빈 값은 허용하지 않습니다.";
+    public static final String REQUIRE_NUMBER = "[ERROR] 숫자를 입력해주세요.";
+    public static final String INVALID_AMOUNT_UNIT = "[ERROR] 금액은 천원단위여야 합니다.";
+    public static final String INVALID_AMOUNT = "[ERROR] 금액은 천원 이상이어야 합니다.";
+    public static final String OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45까지여야 합니다.";
+
+}
